db3d3b7d87031ecc43bcc2f7489acc4b8a955ba0
shreyaiveond <shreya.n@iveond.com>
2022-01-09 15:05:34 +0530

    Add new pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -260,33 +260,15 @@
         $('.active').removeClass('active');
         $(this).addClass('active');
     });
+     
+     
    });	
   /**
    * Scroll down for Number**/
-   var section = document.querySelector('.numbers');
-   var hasEntered = false;
-   
-   window.addEventListener('scroll', (e) => {
-     var shouldAnimate = (window.scrollY + window.innerHeight) >= section.offsetTop;
-   
-     if (shouldAnimate && !hasEntered) {
-       hasEntered = true;
-       
-       $('.value').each(function () {
-         $(this).prop('Counter',0).animate({
-           Counter: $(this).text()
-         }, {
-           duration: 4000,
-           easing: 'swing',
-           step: function (now) {
-               $(this).text(Math.ceil(now));
-           }
-          });
-       });
-   
-     }
-   });
    
   
   // 
 })()
+
+  
+  
